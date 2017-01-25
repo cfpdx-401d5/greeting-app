756568785ee2b0be790da462010f56f5836a012b
Clarefollett <chlaw101@gmail.com>
2017-01-25 11:43:05 -0800

    two tests passing with cowsay

--- test/greet.test.js
+++ test/greet.test.js
@@ -4,7 +4,14 @@ var run = require('./run.js');
 describe('greeting app', function() {
     it('greets by name', function() {
         var output = run(['./lib/greet.js', 'marty']);
-        assert.equal(output, 'hello marty\n');
+        assert.equal(output, ` ______________
+< Hello, marty >
+ --------------
+        \\   ^__^
+         \\  (oO)\\_______
+            (__)\\       )\\/\\
+             U  ||----w |
+                ||     ||\n`);
     });
 
     it('uses "stranger" as a default when no name provided', function() {
