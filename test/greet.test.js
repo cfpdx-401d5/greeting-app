eee60c61321963ea2ef64631876fac86937b1c3f
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-25 10:37:07 -0800

    week2- included cowsay in package.json

--- test/greet.test.js
+++ test/greet.test.js
@@ -2,13 +2,28 @@ var assert = require('assert');
 var run = require('./run.js');
 
 describe('greeting app', function() {
-    it('greets by name', function() {
-        var output = run(['./lib/greet.js', 'marty']);
-        assert.equal(output, 'hello marty\n');
-    });
+//     it('greets by name with cowsay', function() {
+//         var output = run(['./lib/greet.js', 'brigitte']);
+//         assert.equal(output, `
+//  ________________
+// < hello brigitte >
+//  ----------------
+//         \   ^__^
+//          \  (oo)\_______
+//             (__)\       )\/\
+//                 ||----w |
+//                 ||     ||
+//         `
+//         );
+//     });
 
-    it('uses "stranger" as a default when no name provided', function() {
-        var output = run(['./lib/greet.js']);
-        assert.equal(output, 'hello stranger\n');
+    // it('uses "stranger" as a default when no name provided', function() {
+    //     var output = run(['./lib/greet.js']);
+    //     assert.equal(output, 'hello stranger\n');
+    // });
+
+    it('greets with name without cowsay', function() {
+        var output = run(['./lib/greet.js', 'brigitte', '--plain']);
+        assert.equal( output, 'hello brigitte\n');
     });
 });
