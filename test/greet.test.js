708bdc972cc47ee75df345e346a03e95c7fcb126
mericks <mericks@users.noreply.github.com>
2017-01-27 17:04:55 -0800

    Week 2 - cowsay complete

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,14 +1,22 @@
 var assert = require('assert');
 var run = require('./run.js');
+const cowsay = require('cowsay')
 
 describe('greeting app', function() {
-    it('greets by name', function() {
-        var output = run(['./lib/greet.js', 'marty']);
+    
+    it('greets by name --plain', function() {
+        var output = run(['./lib/greet', 'marty', '--plain']);
         assert.equal(output, 'hello marty\n');
     });
 
-    it('uses "stranger" as a default when no name provided', function() {
-        var output = run(['./lib/greet.js']);
+    it('uses "stranger" as a default when no name provided --plain', function() {
+        var output = run(['./lib/greet.js', '--plain']);
         assert.equal(output, 'hello stranger\n');
     });
-});
+
+    it('greets users with cowsay', function() {
+        var output = run(['./lib/greet.js', 'marty']);
+        assert.equal(output, cowsay.say({text: 'hello marty'}) + '\n')
+    }); 
+
+})
