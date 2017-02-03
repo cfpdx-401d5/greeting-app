f32641e71c2048cf1954f7f4cc6f5c5d7697b4c5
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-03 10:41:38 -0800

    added tests for greet.js

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,21 +1,26 @@
 var assert = require('assert');
 var run = require('./run.js');
+const greet = require('../lib/greet');
+const cowsay = require('cowsay');
 
-describe('greeting app', function() {
-//     it('greets by name with cowsay', function() {
-//         var output = run(['./lib/greet.js', 'brigitte']);
-//         assert.equal(output, `
-//  ________________
-// < hello brigitte >
-//  ----------------
-//         \   ^__^
-//          \  (oo)\_______
-//             (__)\       )\/\
-//                 ||----w |
-//                 ||     ||
-//         `
-//         );
-//     });
+describe('greeting app - greet.js', () => {
+    
+    it('greeting at takes three arguments', () => {
+        var output = greet('brigitte', 'hi', true);
+        assert.equal(output, 'hi brigitte');
+    });
+
+    it('checks cowsay', () => {
+        var output = greet('brigitte', 'hi', false);
+        assert.equal(output, 
+        cowsay.say({
+           text : `hi brigitte`
+        }));
+    });
+
+});
+
+describe('greeting app - cli.js', function() {
 
     it('uses "stranger" as a default when no name provided', function() {
         var output = run(['./cli.js', '', '--plain']);
@@ -23,7 +28,13 @@ describe('greeting app', function() {
     });
 
     it('greets with name without cowsay', function() {
-        var output = run(['./cli.js', 'brigitte', '--plain']);
-        assert.equal( output, 'hello brigitte\n');
+        var output = run(['./cli.js', '--name:brigitte', '--plain']);
+        assert.equal(output, 'hello brigitte\n');
     });
+
+    it('greets with specific salutation', () => {
+        var output = run(['./cli.js', '--greeting:hola', '--plain']);
+        assert.equal(output, 'hola stranger\n');
+    });
+
 });
